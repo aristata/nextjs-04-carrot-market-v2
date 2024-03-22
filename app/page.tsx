dd5ef5a96abc3b36d6ff023b282f16fec387781b
aristata <aristatait@gmail.com>
2024-03-22 15:48:56 +0900

    feat: 3.5 responsive modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,28 @@ export default function Home() {
   return (
     <main
       className={
-        "bg-gray-300 h-screen flex items-center justify-center dark:bg-gray-900"
+        "bg-gray-300 h-screen flex items-center justify-center dark:bg-gray-900 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100"
       }
     >
       <div
         className={
-          "flex flex-col space-y-4 bg-white m-10 p-10 rounded-3xl shadow-2xl w-[32rem] dark:bg-gray-600"
+          "flex flex-col space-y-4 bg-white m-10 p-10 rounded-3xl shadow-2xl w-[32rem] dark:bg-gray-600 md:flex-row md:items-center md:space-x-2 md:space-y-0"
         }
       >
-        <div className={"flex justify-between"}>
-          <div className={"flex flex-col"}>
-            <span className={"dark:text-gray-300"}>In transit</span>
-            <span className={"text-2xl font-semibold dark:text-gray-200"}>
-              Coolblue
-            </span>
-          </div>
-          <div className={"bg-orange-400 size-10 rounded-full"}></div>
-        </div>
-        <div className={"mt-2 flex space-x-2"}>
-          <span
-            className={
-              "bg-green-500 text-white text-sm uppercase p-1 px-3 rounded-full hover:bg-green-600 hover:scale-125 transition"
-            }
-          >
-            Today
-          </span>
-          <span className={"font-bold dark:text-gray-300"}>9:30-10:30</span>
-        </div>
-        <div className={"relative"}>
-          <div className={"h-2 w-full bg-gray-200 rounded-full absolute"}></div>
-          <div className={"h-2 w-2/3 bg-green-500 rounded-full absolute"}></div>
-        </div>
-        <div
+        <input
+          type="text"
+          placeholder={"search"}
           className={
-            "flex justify-between items-center mt-5 text-gray-600 dark:text-gray-200"
+            "w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          }
+        />
+        <button
+          className={
+            "bg-black text-white py-2 rounded-full active:scale-95 transition-transform font-medium focus:scale-95 outline-none md:p-2 md:h-12 md:px-4"
           }
         >
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className={"text-gray-400"}>Delivered</span>
-        </div>
+          search
+        </button>
       </div>
     </main>
   );
